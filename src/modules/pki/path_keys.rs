a84d74582b90021ac613f17670cd047bc877fdfa
Jin Jiu <zuxi.wzx@alibaba-inc.com>
2023-11-23 16:14:15 +0800

    The implementation of various functionalities in the PKI module.
    1. Key Generation: Provides a api to generate cryptographic keys.
    2. Key Import: It allows importing existing keys into the pki module.
    3. Encryption and Decryption of Symmetric Keys: Supports encryption and decryption operations using symmetric keys.
    4. Signing and Verification of Asymmetric Keys: It includes functionalities to sign and verify data using asymmetric keys.

---
+++ src/modules/pki/path_keys.rs
@@ -0,0 +1,246 @@
+use openssl::{
+    rsa::{Rsa},
+    ec::{EcKey},
+};
+use serde_json::{json, Value};
+use crate::{
+    utils::key::KeyBundle,
+    logical::{
+        Backend, Request, Response,
+    },
+    storage::{StorageEntry},
+    errors::RvError,
+};
+use super::{
+    PkiBackendInner,
+};
+
+const PKI_CONFIG_KEY_PREFIX: &str = "config/key/";
+
+impl PkiBackendInner {
+    pub fn generate_key(&self, _backend: &dyn Backend, req: &mut Request) -> Result<Option<Response>, RvError> {
+        let key_name_value = req.get_data("key_name")?;
+        let key_name = key_name_value.as_str().unwrap();
+        let key_type_value = req.get_data("key_type")?;
+        let key_type = key_type_value.as_str().unwrap();
+        let key_bits_value = req.get_data("key_bits")?;
+        let key_bits = key_bits_value.as_u64().unwrap();
+
+        let mut export_private_key = false;
+        if req.path.ends_with("/exported") {
+            export_private_key = true;
+        }
+
+        let key_info = self.fetch_key(req, key_name);
+        if key_info.is_ok() {
+            return Err(RvError::ErrPkiKeyNameAlreadyExist);
+        }
+
+        let mut key_bundle = KeyBundle::new(key_name, key_type, key_bits as u32);
+        key_bundle.generate()?;
+
+        self.write_key(req, &key_bundle)?;
+
+        let mut resp_data = json!({
+            "key_id": key_bundle.id.clone(),
+            "key_name": key_bundle.name.clone(),
+            "key_type": key_bundle.key_type.clone(),
+            "key_bits": key_bundle.bits,
+        }).as_object().unwrap().clone();
+
+        if export_private_key {
+            match key_type {
+                "rsa" | "ec" => {
+                    resp_data.insert("private_key".to_string(), Value::String(String::from_utf8_lossy(&key_bundle.key).to_string()));
+                },
+                _ => {
+                    resp_data.insert("private_key".to_string(), Value::String(hex::encode(&key_bundle.key)));
+                }
+            }
+
+            if key_bundle.iv.len() > 0 {
+                resp_data.insert("iv".to_string(), Value::String(hex::encode(&key_bundle.iv)));
+            }
+        }
+
+        Ok(Some(Response::data_response(Some(resp_data))))
+    }
+
+    pub fn import_key(&self, _backend: &dyn Backend, req: &mut Request) -> Result<Option<Response>, RvError> {
+        let key_name_value = req.get_data("key_name")?;
+        let key_name = key_name_value.as_str().unwrap();
+        let key_type_value = req.get_data("key_type")?;
+        let key_type = key_type_value.as_str().unwrap();
+        let pem_bundle_value = req.get_data("pem_bundle");
+        let hex_bundle_value = req.get_data("hex_bundle");
+        if pem_bundle_value.is_err() && hex_bundle_value.is_err() {
+            return Err(RvError::ErrRequestFieldNotFound);
+        }
+
+        let key_info = self.fetch_key(req, key_name);
+        if key_info.is_ok() {
+            return Err(RvError::ErrPkiKeyNameAlreadyExist);
+        }
+
+        let mut key_bundle = KeyBundle::new(key_name, key_type, 0);
+
+        match pem_bundle_value {
+            Ok(pem_bundle_val) => {
+                if let Some(pem_bundle) = pem_bundle_val.as_str() {
+                    key_bundle.key = pem_bundle.as_bytes().to_vec();
+                    match key_type {
+                        "rsa" => {
+                            let rsa = Rsa::private_key_from_pem(&key_bundle.key)?;
+                            key_bundle.bits = rsa.size() * 8;
+                        },
+                        "ec" => {
+                            let ec_key = EcKey::private_key_from_pem(&key_bundle.key)?;
+                            key_bundle.bits = ec_key.group().degree();
+                        },
+                        _ => {
+                            return Err(RvError::ErrPkiKeyTypeInvalid);
+                        }
+                    }
+                }
+            }
+            _ => {}
+        }
+
+        match hex_bundle_value {
+            Ok(hex_bundle_val) => {
+                if let Some(hex_bundle) = hex_bundle_val.as_str() {
+                    key_bundle.key = hex::decode(&hex_bundle)?;
+                    key_bundle.bits = (key_bundle.key.len() as u32) * 8;
+                    match key_bundle.bits {
+                        128 | 192 | 256 => {},
+                        _ => {
+                            return Err(RvError::ErrPkiKeyBitsInvalid);
+                        }
+                    }
+                    let iv_value = req.get_data("iv")?;
+                    match key_type {
+                        "aes-gcm" | "aes-cbc" => {
+                            if let Some(iv) = iv_value.as_str() {
+                                key_bundle.iv = hex::decode(&iv)?;
+                            } else {
+                                return Err(RvError::ErrRequestFieldNotFound);
+                            }
+                        },
+                        "aes-ecb" => {},
+                        _ => {
+                            return Err(RvError::ErrPkiKeyTypeInvalid);
+                        }
+                    }
+                }
+            }
+            _ => {}
+        }
+
+        self.write_key(req, &key_bundle)?;
+
+        let resp_data = json!({
+            "key_id": key_bundle.id.clone(),
+            "key_name": key_bundle.name.clone(),
+            "key_type": key_bundle.key_type.clone(),
+            "key_bits": key_bundle.bits,
+        }).as_object().unwrap().clone();
+
+        Ok(Some(Response::data_response(Some(resp_data))))
+    }
+
+    pub fn key_sign(&self, _backend: &dyn Backend, req: &mut Request) -> Result<Option<Response>, RvError> {
+        let key_name_value = req.get_data("key_name")?;
+        let key_name = key_name_value.as_str().unwrap();
+        let data_value = req.get_data("data")?;
+        let data = data_value.as_str().unwrap();
+
+        let key_bundle = self.fetch_key(req, key_name)?;
+
+        let decoded_data = hex::decode(data.as_bytes())?;
+        let result = key_bundle.sign(&decoded_data)?;
+
+        let resp_data = json!({
+            "result": hex::encode(&result),
+        }).as_object().unwrap().clone();
+
+        Ok(Some(Response::data_response(Some(resp_data))))
+    }
+
+    pub fn key_verify(&self, _backend: &dyn Backend, req: &mut Request) -> Result<Option<Response>, RvError> {
+        let key_name_value = req.get_data("key_name")?;
+        let key_name = key_name_value.as_str().unwrap();
+        let data_value = req.get_data("data")?;
+        let data = data_value.as_str().unwrap();
+        let signature_value = req.get_data("signature")?;
+        let signature = signature_value.as_str().unwrap();
+
+        let key_bundle = self.fetch_key(req, key_name)?;
+
+        let decoded_data = hex::decode(data.as_bytes())?;
+        let decoded_signature = hex::decode(signature.as_bytes())?;
+        let result = key_bundle.verify(&decoded_data, &decoded_signature)?;
+
+        let resp_data = json!({
+            "result": result,
+        }).as_object().unwrap().clone();
+
+        Ok(Some(Response::data_response(Some(resp_data))))
+    }
+
+    pub fn key_encrypt(&self, _backend: &dyn Backend, req: &mut Request) -> Result<Option<Response>, RvError> {
+        let key_name_value = req.get_data("key_name")?;
+        let key_name = key_name_value.as_str().unwrap();
+        let data_value = req.get_data("data")?;
+        let data = data_value.as_str().unwrap();
+        let aad_value = req.get_data("aad")?;
+        let aad = aad_value.as_str().unwrap();
+
+        let key_bundle = self.fetch_key(req, key_name)?;
+
+        let decoded_data = hex::decode(data.as_bytes())?;
+        let result = key_bundle.encrypt(&decoded_data, Some(aad.as_bytes()))?;
+
+        let resp_data = json!({
+            "result": hex::encode(&result),
+        }).as_object().unwrap().clone();
+
+        Ok(Some(Response::data_response(Some(resp_data))))
+    }
+
+    pub fn key_decrypt(&self, _backend: &dyn Backend, req: &mut Request) -> Result<Option<Response>, RvError> {
+        let key_name_value = req.get_data("key_name")?;
+        let key_name = key_name_value.as_str().unwrap();
+        let data_value = req.get_data("data")?;
+        let data = data_value.as_str().unwrap();
+        let aad_value = req.get_data("aad")?;
+        let aad = aad_value.as_str().unwrap();
+
+        let key_bundle = self.fetch_key(req, key_name)?;
+
+        let decoded_data = hex::decode(data.as_bytes())?;
+        let result = key_bundle.decrypt(&decoded_data, Some(aad.as_bytes()))?;
+
+        let resp_data = json!({
+            "result": hex::encode(&result),
+        }).as_object().unwrap().clone();
+
+        Ok(Some(Response::data_response(Some(resp_data))))
+    }
+
+    pub fn fetch_key(&self, req: &Request, key_name: &str) -> Result<KeyBundle, RvError> {
+        let entry = req.storage_get(format!("{}{}", PKI_CONFIG_KEY_PREFIX, key_name).as_str())?;
+        if entry.is_none() {
+            return Err(RvError::ErrPkiCertNotFound);
+        }
+
+        let key_bundle: KeyBundle = serde_json::from_slice(entry.unwrap().value.as_slice())?;
+        Ok(key_bundle)
+    }
+
+    pub fn write_key(&self, req: &Request, key_bundle: &KeyBundle) -> Result<(), RvError> {
+        let key_name = format!("{}{}", PKI_CONFIG_KEY_PREFIX, key_bundle.name);
+        let entry = StorageEntry::new(key_name.as_str(), key_bundle)?;
+        req.storage_put(&entry)?;
+        Ok(())
+    }
+}
---
+++ src/utils/key.rs
@@ -0,0 +1,323 @@
+use openssl::{
+    pkey::{PKey},
+    rsa::{Rsa, Padding},
+    ec::{EcGroup, EcKey},
+	nid::Nid,
+    rand::rand_bytes,
+    hash::MessageDigest,
+    sign::{Signer, Verifier},
+    symm::{Cipher, encrypt, decrypt, encrypt_aead, decrypt_aead},
+};
+use serde::{Serialize, Deserialize};
+use crate::{
+    utils::generate_uuid,
+    errors::RvError,
+};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct KeyBundle {
+    pub id: String,
+    pub name: String,
+    pub key_type: String,
+    pub key: Vec<u8>,
+    //for aes-gcm | aes-cbc
+    pub iv: Vec<u8>,
+    pub bits: u32,
+}
+
+impl Default for KeyBundle {
+    fn default() -> Self {
+        KeyBundle {
+            id: generate_uuid(),
+            name: String::new(),
+            key_type: String::new(),
+			key: Vec::new(),
+			iv: Vec::new(),
+            bits: 0,
+        }
+    }
+}
+
+impl KeyBundle {
+    pub fn new(name: &str, key_type: &str, key_bits: u32) -> Self {
+        Self {
+            name: name.to_string(),
+            key_type: key_type.to_string(),
+            bits: key_bits,
+            ..KeyBundle::default()
+        }
+    }
+
+    pub fn generate(&mut self) -> Result<(), RvError> {
+        let key_bits = self.bits;
+        let priv_key = match self.key_type.as_str() {
+            "rsa" => {
+                if key_bits != 2048 && key_bits != 3072 && key_bits != 4096 {
+                    return Err(RvError::ErrPkiKeyBitsInvalid);
+                }
+                let rsa_key = Rsa::generate(key_bits)?;
+                let pkey = PKey::from_rsa(rsa_key)?;
+                pkey.private_key_to_pem_pkcs8()?
+            },
+            "ec" => {
+                let curve_name = match key_bits {
+                    224 => Nid::SECP224R1,
+                    256 => Nid::SECP256K1,
+                    384 => Nid::SECP384R1,
+                    521 => Nid::SECP521R1,
+                    _ => {
+                        return Err(RvError::ErrPkiKeyBitsInvalid);
+                    }
+                };
+                let ec_group = EcGroup::from_curve_name(curve_name)?;
+                let ec_key = EcKey::generate(ec_group.as_ref())?;
+                let pkey = PKey::from_ec_key(ec_key)?;
+                pkey.private_key_to_pem_pkcs8()?
+            },
+            "aes-gcm" | "aes-cbc" | "aes-ecb" => {
+                if key_bits != 128 && key_bits != 192 && key_bits != 256 {
+                    return Err(RvError::ErrPkiKeyBitsInvalid);
+                }
+
+                if self.key_type.as_str() != "aes-ecb" {
+                    let mut iv_bytes = vec![0u8; 16];
+                    rand_bytes(&mut iv_bytes)?;
+                    self.iv = iv_bytes;
+                }
+
+                let mut random_bytes = vec![0u8; (key_bits/8) as usize];
+                rand_bytes(&mut random_bytes)?;
+                random_bytes
+            },
+            _ => {
+                return Err(RvError::ErrPkiKeyTypeInvalid);
+            }
+        };
+
+        self.key = priv_key;
+
+        Ok(())
+    }
+
+    pub fn sign(&self, data: &[u8]) -> Result<Vec<u8>, RvError> {
+        match self.key_type.as_str() {
+            "rsa" => {
+                let rsa = Rsa::private_key_from_pem(&self.key)?;
+                let pkey = PKey::from_rsa(rsa)?;
+                let mut signer = Signer::new(MessageDigest::sha256(), &pkey)?;
+                signer.set_rsa_padding(Padding::PKCS1)?;
+                signer.update(data)?;
+                return Ok(signer.sign_to_vec()?);
+            },
+            "ec" => {
+				let ec_key = EcKey::private_key_from_pem(&self.key)?;
+                let pkey = PKey::from_ec_key(ec_key)?;
+				let mut signer = Signer::new(MessageDigest::sha256(), &pkey)?;
+                signer.update(data)?;
+                return Ok(signer.sign_to_vec()?);
+            },
+            _ => {
+                return Err(RvError::ErrPkiKeyOperationInvalid);
+            }
+        }
+    }
+
+    pub fn verify(&self, data: &[u8], signature: &[u8]) -> Result<bool, RvError> {
+        match self.key_type.as_str() {
+            "rsa" => {
+                let rsa = Rsa::private_key_from_pem(&self.key)?;
+                let pkey = PKey::from_rsa(rsa)?;
+                let mut verifier = Verifier::new(MessageDigest::sha256(), &pkey)?;
+                verifier.set_rsa_padding(Padding::PKCS1)?;
+                verifier.update(data)?;
+                return Ok(verifier.verify(signature).unwrap_or(false));
+            },
+            "ec" => {
+				let ec_key = EcKey::private_key_from_pem(&self.key)?;
+                let pkey = PKey::from_ec_key(ec_key)?;
+                let mut verifier = Verifier::new(MessageDigest::sha256(), &pkey)?;
+                verifier.update(data)?;
+                return Ok(verifier.verify(signature).unwrap_or(false));
+            },
+            _ => {
+                return Err(RvError::ErrPkiKeyOperationInvalid);
+            }
+        }
+    }
+
+    pub fn encrypt(&self, data: &[u8], aad: Option<&[u8]>) -> Result<Vec<u8>, RvError> {
+        match self.key_type.as_str() {
+            "aes-gcm" => {
+                let cipher = match self.bits {
+                    128 => Cipher::aes_128_gcm(),
+                    192 => Cipher::aes_192_gcm(),
+                    256 => Cipher::aes_256_gcm(),
+                    _ => {
+                        return Err(RvError::ErrPkiKeyBitsInvalid);
+                    }
+                };
+                let mut tag = vec![0u8; 16];
+                let mut ciphertext = encrypt_aead(cipher, &self.key, Some(&self.iv), aad.unwrap_or("".as_bytes()), data, &mut tag)?;
+                ciphertext.extend_from_slice(&tag);
+                Ok(ciphertext)
+
+            },
+            "aes-cbc" => {
+                let cipher = match self.bits {
+                    128 => Cipher::aes_128_cbc(),
+                    192 => Cipher::aes_192_cbc(),
+                    256 => Cipher::aes_256_cbc(),
+                    _ => {
+                        return Err(RvError::ErrPkiKeyBitsInvalid);
+                    }
+                };
+
+                Ok(encrypt(cipher, &self.key, Some(&self.iv), data)?)
+            },
+            "aes-ecb" => {
+                let cipher = match self.bits {
+                    128 => Cipher::aes_128_ecb(),
+                    192 => Cipher::aes_192_ecb(),
+                    256 => Cipher::aes_256_ecb(),
+                    _ => {
+                        return Err(RvError::ErrPkiKeyBitsInvalid);
+                    }
+                };
+
+                Ok(encrypt(cipher, &self.key, None, data)?)
+            },
+            _ => {
+                return Err(RvError::ErrPkiKeyOperationInvalid);
+            }
+        }
+    }
+
+    pub fn decrypt(&self, data: &[u8], aad: Option<&[u8]>) -> Result<Vec<u8>, RvError> {
+        match self.key_type.as_str() {
+            "aes-gcm" => {
+                let cipher = match self.bits {
+                    128 => Cipher::aes_128_gcm(),
+                    192 => Cipher::aes_192_gcm(),
+                    256 => Cipher::aes_256_gcm(),
+                    _ => {
+                        return Err(RvError::ErrPkiKeyBitsInvalid);
+                    }
+                };
+                let (ciphertext, tag) = data.split_at(data.len() - 16);
+                Ok(decrypt_aead(cipher, &self.key, Some(&self.iv), aad.unwrap_or("".as_bytes()), ciphertext, tag)?)
+
+            },
+            "aes-cbc" => {
+                let cipher = match self.bits {
+                    128 => Cipher::aes_128_cbc(),
+                    192 => Cipher::aes_192_cbc(),
+                    256 => Cipher::aes_256_cbc(),
+                    _ => {
+                        return Err(RvError::ErrPkiKeyBitsInvalid);
+                    }
+                };
+
+                Ok(decrypt(cipher, &self.key, Some(&self.iv), data)?)
+            },
+            "aes-ecb" => {
+                let cipher = match self.bits {
+                    128 => Cipher::aes_128_ecb(),
+                    192 => Cipher::aes_192_ecb(),
+                    256 => Cipher::aes_256_ecb(),
+                    _ => {
+                        return Err(RvError::ErrPkiKeyBitsInvalid);
+                    }
+                };
+
+                Ok(decrypt(cipher, &self.key, None, data)?)
+            },
+            _ => {
+                return Err(RvError::ErrPkiKeyOperationInvalid);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn test_key_sign_verify(key_bundle: &mut KeyBundle) {
+        assert!(key_bundle.generate().is_ok());
+        let data = "123456789";
+        let signature = key_bundle.sign(data.as_bytes());
+        assert!(signature.is_ok());
+        assert!(signature.as_ref().unwrap().len() > data.len());
+        let verify = key_bundle.verify(data.as_bytes(), signature.as_ref().unwrap());
+        assert!(verify.is_ok());
+        assert!(verify.unwrap());
+    }
+
+    fn test_key_encrypt_decrypt(key_bundle: &mut KeyBundle, aad: Option<&[u8]>) {
+        assert!(key_bundle.generate().is_ok());
+        let data = "123456789";
+        let result = key_bundle.encrypt(data.as_bytes(), aad);
+        assert!(result.is_ok());
+        let encrypted_data = result.unwrap();
+        let result = key_bundle.decrypt(&encrypted_data, aad);
+        assert!(result.is_ok());
+        let decrypted_data = result.unwrap();
+        assert_eq!(std::str::from_utf8(&decrypted_data).unwrap(), data);
+    }
+
+    #[test]
+    fn test_rsa_key_operation() {
+        let mut key_bundle = KeyBundle::new("rsa-2048", "rsa", 2048);
+        test_key_sign_verify(&mut key_bundle);
+        let mut key_bundle = KeyBundle::new("rsa-3072", "rsa", 3072);
+        test_key_sign_verify(&mut key_bundle);
+        let mut key_bundle = KeyBundle::new("rsa-4096", "rsa", 4096);
+        test_key_sign_verify(&mut key_bundle);
+    }
+
+    #[test]
+    fn test_ec_key_operation() {
+        let mut key_bundle = KeyBundle::new("ec-224", "ec", 224);
+        test_key_sign_verify(&mut key_bundle);
+        let mut key_bundle = KeyBundle::new("ec-256", "ec", 256);
+        test_key_sign_verify(&mut key_bundle);
+        let mut key_bundle = KeyBundle::new("ec-384", "ec", 384);
+        test_key_sign_verify(&mut key_bundle);
+        let mut key_bundle = KeyBundle::new("ec-521", "ec", 521);
+        test_key_sign_verify(&mut key_bundle);
+    }
+
+    #[test]
+    fn test_aes_key_operation() {
+        // test aes-gcm
+        let mut key_bundle = KeyBundle::new("aes-gcm-128", "aes-gcm", 128);
+        test_key_encrypt_decrypt(&mut key_bundle, None);
+        test_key_encrypt_decrypt(&mut key_bundle, None);
+        test_key_encrypt_decrypt(&mut key_bundle, Some("rusty_vault".as_bytes()));
+        let mut key_bundle = KeyBundle::new("aes-gcm-192", "aes-gcm", 192);
+        test_key_encrypt_decrypt(&mut key_bundle, None);
+        test_key_encrypt_decrypt(&mut key_bundle, None);
+        test_key_encrypt_decrypt(&mut key_bundle, Some("rusty_vault".as_bytes()));
+        let mut key_bundle = KeyBundle::new("aes-gcm-256", "aes-gcm", 256);
+        test_key_encrypt_decrypt(&mut key_bundle, None);
+        test_key_encrypt_decrypt(&mut key_bundle, Some("rusty_vault".as_bytes()));
+        test_key_encrypt_decrypt(&mut key_bundle, None);
+        test_key_encrypt_decrypt(&mut key_bundle, Some("rusty_vault".as_bytes()));
+
+        // test aes-cbc
+        let mut key_bundle = KeyBundle::new("aes-cbc-128", "aes-cbc", 128);
+        test_key_encrypt_decrypt(&mut key_bundle, None);
+        let mut key_bundle = KeyBundle::new("aes-cbc-192", "aes-cbc", 192);
+        test_key_encrypt_decrypt(&mut key_bundle, None);
+        let mut key_bundle = KeyBundle::new("aes-cbc-256", "aes-cbc", 256);
+        test_key_encrypt_decrypt(&mut key_bundle, None);
+
+        // test aes-ecb
+        let mut key_bundle = KeyBundle::new("aes-ecb-128", "aes-ecb", 128);
+        test_key_encrypt_decrypt(&mut key_bundle, None);
+        let mut key_bundle = KeyBundle::new("aes-ecb-192", "aes-ecb", 192);
+        test_key_encrypt_decrypt(&mut key_bundle, None);
+        let mut key_bundle = KeyBundle::new("aes-ecb-256", "aes-ecb", 256);
+        test_key_encrypt_decrypt(&mut key_bundle, None);
+    }
+}
